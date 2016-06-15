7ecaccd9340e7c0b6407becec75dd1c698058671
Cairo Martins <martins.cairo@gmail.com>
2016-06-15 09:32:22 -0300

    Completo

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 //                              C++ Includes 
 //==============================================================================
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
 
@@ -21,189 +20,210 @@
 #include <Volumes.h>
 
 //==============================================================================
+//                               Other includes 
+//==============================================================================
+#include <libconfig.h++>
+
+//==============================================================================
+//                                 typedef
+//==============================================================================]
+typedef std :: vector <std::string>                   vecString;
+typedef std :: vector <Real>                          vecReal;
+
+
+//==============================================================================
 //                                prototypes
 //==============================================================================
-Real CalculaDistFaces (Volumes&);
-void print (Real x[], const int &, const std::string&); 
-void CompletaDiagPrinc (Volumes&, Real x[]);
-void CompletaDiagLeste (Volumes&, Real x[]);
-void CompletaDiagOeste (Volumes&, Real x[]);
-void CompletaVetorSolucao (Volumes&, Real x[]);
+const int LeituraDadosProblema (Volumes&);
+
+void GeracaoMalha (vecReal&, vecReal&, vecReal&, vecReal&, Volumes&);
+
+void CalculaCoeficientes (vecReal&, vecReal&, vecReal&, vecReal&,
+                          vecReal&, vecReal&);
+
+
 
 //==============================================================================
 //                              main function
 //==============================================================================
 
 int main(int argc, char** argv)
-{
+{  
     
-//------------------------------------------------------------------------------
-//                 Leitura do arquivo de configuracao Volumes.txt
-//------------------------------------------------------------------------------
-           
-    std :: ifstream input("Volumes.txt"); //O ARQUIVO PRECISA EXISTIR     
-    if (!input)
-    {
-        std :: cerr << "Arquivo inexistente" << std :: endl;
-        exit (EXIT_FAILURE);
-    }
 //------------------------------------------------------------------------------
 //                    Assimilando dados do arquivo externo
 //------------------------------------------------------------------------------
-    Volumes v1;
-    input >> v1;      //importanto arquivos de dados
-    std::cout << "v1:" << v1 << std :: endl;
-    input.close();    //fechando arquivo de dados
+    Volumes v1;    
+    LeituraDadosProblema (v1);
     
-//------------------------------------------------------------------------------
-//                             Calculando distâncias
-//------------------------------------------------------------------------------    
-    
-    Real DistVolumes,  //distância entre volumes adjacentes
-         DistCentros;  //distância entre os centros de volumes adjacentes
-    
-    DistVolumes = CalculaDistFaces (v1);
-    DistCentros = DistVolumes;         
-//volumes homogêneos fazem com quea as duas distâncias em questão sejam iguais
- 
- 
-//------------------------------------------------------------------------------
-//                             Declarando vetores
-//------------------------------------------------------------------------------
-    Real *Ap, *Ae, *Aw, *Sp;  //declaração de ponteiros
-     
-    Ap = new Real [v1.NVOL()]; //DIAGONAL PRINCIPAL, CONTENDO DE REAIS
-    Ae = new Real [v1.NVOL()]; //DIAGONAL LESTE,     CONTENDO DE REAIS
-    Aw = new Real [v1.NVOL()]; //DIAGONAL OESTE,     CONTENDO DE REAIS
-    Sp = new Real [v1.NVOL()]; //VETOR DE SOLUÇÃO,   CONTENDO DE REAIS
-    
-//------------------------------------------------------------------------------
-//                            Completando vetores
-//------------------------------------------------------------------------------
-    
-//______________ 1. completando vetores diagonal principal______________________   
-    
-    std:: cout << "Imprimindo vetor da diagonal principal" << std :: endl;    
-    CompletaDiagPrinc (v1, Ap);    
-    std :: string NOMEVETOR("Ap"); 
-    //cria variável do tipo string, que auxiliará na impressão do vetor
-    
-    print (Ap, v1.NVOL(), NOMEVETOR);
-    
-        
-//_________________2. completando vetores diagonal oeste________________________
-    
-   std:: cout << "\n\nImprimindo vetor da diagonal oeste" << std :: endl;    
-   CompletaDiagOeste (v1, Aw);
-   NOMEVETOR.erase(1,2);
-   //delete a string anterior associada à variável NOMEVETOR, para redefini-la
+    std :: vector <Real>      xFronteiras (v1.NVOL()+1), //localizações fronteiras
+                              xCentro     (v1.NVOL()),   //localizações centros
+                              DistCentro  (v1.NVOL()+1), //distâncias entre centros
+                              DistFace    (v1.NVOL());   //distância entre fronteiras adjacentes
    
-   NOMEVETOR="Aw";
-   print (Aw, v1.NVOL(), NOMEVETOR);
 
-
-//_______________3. completando vetores diagonal leste__________________________
-   
-   std:: cout << "\n\nImprimindo vetor da diagonal leste" << std :: endl;         
-   CompletaDiagLeste (v1, Ae);
-   NOMEVETOR.erase(1,2);
-   NOMEVETOR="Ae";
-   print (Ae, v1.NVOL(), NOMEVETOR);
-
-
-//___________________4. completando vetores resultados__________________________   
-   
-   std:: cout << "\n\nImprimindo vetor de resultados" << std :: endl;         
-   CompletaVetorSolucao (v1, Sp);
-   NOMEVETOR.erase(1,2);
-   NOMEVETOR="Sp";
-   print (Sp, v1.NVOL(), NOMEVETOR);
+    GeracaoMalha (xFronteiras, xCentro, DistCentro, DistFace, v1);
     
-
-//------------------------------------------------------------------------------
-//                   Deletando memória alocada pelos vetores
-//------------------------------------------------------------------------------
-    delete [] Ap;
-    delete [] Ae;
-    delete [] Aw;
-    delete [] Sp;
+    std :: vector <Real>      Ap(v1.NVOL()),
+                              Ae(v1.NVOL()),
+                              Aw(v1.NVOL()),
+                              Sp(v1.NVOL());
     
+    CalculaCoeficientes (DistFace, DistCentro, Ap, Ae, Aw, Sp);
     
-}
-
-
+    for (int i=0; i<v1.NVOL(); i++)
+    {
+    std :: cout << "Ae[" << i << "]=" << Ap[i] << std :: endl;
+    } 
+    
+}    
 //==============================================================================
 //                           funções auxiliares
 //==============================================================================
 
 //------------------------------------------------------------------------------
-//                1. Calculando distância entre volumes adjacentes
+//                  1. Função para ler dados dos problemas
 //------------------------------------------------------------------------------
-Real CalculaDistFaces (Volumes& _vol)
-{
-    return _vol.COMPRIMENTO()/_vol.NVOL();
-}
-
-//------------------------------------------------------------------------------
-//                2. Completando vetor da diagonal principal
-//------------------------------------------------------------------------------
-void CompletaDiagPrinc (Volumes& _vol, Real x[])
-{
-    for (int i=0; i<_vol.NVOL(); i++)
-    {
-        if (i==0 || i== _vol.NVOL()-1)  x[i]=3;
-        else x[i]=2;
-    }
-}    
-
-//------------------------------------------------------------------------------
-//                3. Completando vetor da diagonal Oeste
-//------------------------------------------------------------------------------
-void CompletaDiagOeste (Volumes& _vol, Real x[])
-{
-
-    for (int i=0; i<_vol.NVOL(); i++)
-    {
-        if (i==0) x[i]=0;
-        else x[i]=-1;
-    }
-}
-
-//------------------------------------------------------------------------------
-//                4. Completando vetores da diagonal leste
-//------------------------------------------------------------------------------
-void CompletaDiagLeste (Volumes& _vol, Real x[])
-{
-
-    for (int i=0; i<_vol.NVOL(); i++)
-    {
-        if (i==_vol.NVOL()-1) x[i]=0;
-        else x[i]=-1;
-    }
-}
-
-//------------------------------------------------------------------------------
-//                     5. Completando vetor solução
-//------------------------------------------------------------------------------
-void CompletaVetorSolucao(Volumes& _vol, Real x[])
-{
-    for (int i=0; i<_vol.NVOL(); i++)
-    {
-        if(i==0) x[i]=2*_vol.IMGESQUERDA();
-        else if (i==_vol.NVOL()-1) x[i]=2*_vol.IMGDIREITA();
-        else x[i]=0;
-    }
-}
-
-//------------------------------------------------------------------------------
-//                     6. Função para impressão de vetores
-//------------------------------------------------------------------------------
-void print (Real x[], int const & _a, const std:: string & _nome) 
-{
-   for (int i=0; i< _a; i++)
-   {
-       std::cout << _nome << "[" <<i << "]="  << x[i] << std::endl;
-   }
-}
-
     
+ const int LeituraDadosProblema (Volumes& _vol)
+ {
+     libconfig :: Config        cfg_txt;
+     const std :: string        FILENAME("Volumes.txt");
+     
+     try
+     {
+         cfg_txt.readFile (FILENAME.c_str());         
+     }
+     
+     catch (const libconfig :: FileIOException &fioex)
+     {
+         std :: cerr << "Arquivo "
+                     << FILENAME
+                     << " não foi encontrado. \n"
+                     << "Execução cancelada. \n";
+         return (EXIT_FAILURE);
+     }
+     
+     
+     catch (const libconfig :: ParseException &pex)
+     {
+         std :: cerr << "Erro no arquivo: " << pex.getFile()
+                     << "\nNa linha: "      << pex.getLine()
+                     << "\nErro: "          << pex.getError() 
+                     << std :: endl;
+         
+         return (EXIT_FAILURE);
+     }
+     
+ const vecString                      vecDados = {"Comprimento",
+                                                  "Abscissa_inicial",
+                                                  "Imagem_esquerda",
+                                                  "Imagem_direita",
+                                                  "Numero_Volumes"
+                                                  };
+
+     
+     try
+     {
+         for (auto &ItVecDados : vecDados)
+         {
+             cfg_txt.lookup(ItVecDados);
+         }
+     }  
+     
+         catch (const libconfig :: SettingNotFoundException &nfex)
+         {
+             std :: cerr << "Parâmetro não encontrado: " 
+                         << nfex.getPath() << "\n" 
+                         << nfex.what() << std :: endl;
+         }
+         
+     
+     _vol.comprimento    = cfg_txt.lookup(vecDados[0]);
+     _vol.xFace          = cfg_txt.lookup(vecDados[1]);
+     _vol.imagemEsquerda = cfg_txt.lookup(vecDados[2]);
+     _vol.imagemDireita  = cfg_txt.lookup(vecDados[3]);     
+     _vol.nVol           = cfg_txt.lookup(vecDados[4]);
+     
+          
+     std :: cout << "Comprimento do domínio: "       << _vol.comprimento
+                 << "\nNúmero de Volumes na malha: " << _vol.nVol
+                 << "\nAbscissa da face inicial: "   << _vol.xFace
+                 <<"\nImagem à esquerda: "           << _vol.imagemEsquerda
+                 << "\nImagem à direita: "            << _vol.imagemDireita
+                 << std :: endl;
+ }
+ 
+ 
+ 
+ 
+void GeracaoMalha(vecReal& xfronteira, vecReal& xcentro, vecReal& distcentro,
+                  vecReal& distface, Volumes& _vol) 
+ {
+     Real DistFronteiras(_vol.comprimento/_vol.nVol), 
+          DistCentros (DistFronteiras);
+     
+ //___________________1. Completando vetores de fronteiras______________________
+     
+     for (int i=0; i<_vol.NVOL() +1; i++)
+     {
+        if (i==0) xfronteira[0] = _vol.XFACE();
+        
+        else xfronteira[i] = xfronteira[i-1] + DistFronteiras;
+     }
+   
+//_______________2.Completando vetores coordenadas do centro____________________
+     
+     for (int i=0; i<_vol.NVOL(); i++)
+     {
+            xcentro[i] = (xfronteira[i+1] +xfronteira[i])*0.5;
+     }
+     
+//_____________3. Completando vetores com distâncias entre centros______________
+     
+    for (int i=0; i<_vol.NVOL()+1; i++) 
+    {
+        if (i==0)  distcentro[i] = xcentro[i] - xfronteira[i];
+        
+        else if(i==_vol.NVOL()) distcentro[i] = xfronteira[i] - xcentro[i-1];
+        
+        else distcentro[i] = xcentro[i]- xcentro[i-1];
+      
+    }
+     
+     
+//______________4.Completando vetor distância entre fronteiras__________________
+     
+     for (int i=0; i<_vol.NVOL(); i++)
+     {
+         distface[i] = xfronteira[i+1] - xfronteira[i];
+     }    
+}
+
+ 
+
+void CalculaCoeficientes (vecReal& distf, vecReal& distc, vecReal& ap, 
+                          vecReal& ae, vecReal& aw, vecReal& sp)
+{
+    
+//__________________1.Completando vetor diagonal oeste__________________________
+    for (int i=0; i<distf.size(); i++)
+    {
+       if (i==0) aw[i] = 0;
+       else   aw[i] = -1/distc[i];
+    }
+ 
+//__________________2.Completando vetor diagonal leste__________________________
+   for (int i=0; i<distf.size(); i++)
+   {
+       if (i==distf.size()-1) ae[i]=0;
+       else ae[i] = -1/distc[i+1];
+   }
+  
+//__________________1.Completando vetor diagonal principal______________________
+    for (int i=0; i<distf.size(); i++)
+    {
+        ap[i] = (1/distc[i+1]) + (1/distc[i]);
+    }   
+    
+}
