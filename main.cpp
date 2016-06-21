2c23ca3da53f736d54ccbaf236140801794ca5ef
Cairo Martins <martins.cairo@gmail.com>
2016-06-21 13:46:18 -0300

    Completo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Name        : main.cpp
 // Author      : Cairo Martins e Leonardo Thimoteo
 // Version     : 1.0
-// Description : Program to develop the finite volumes method
+// Description : Program to develop the tridimensional matrix algorithm
 // Status      : Needs verification
 //==============================================================================
 
@@ -30,16 +30,25 @@
 typedef std :: vector <std::string>                   vecString;
 typedef std :: vector <Real>                          vecReal;
 
-
 //==============================================================================
 //                                prototypes
 //==============================================================================
+
 const int LeituraDadosProblema (Volumes&);
 
 void GeracaoMalha (vecReal&, vecReal&, vecReal&, vecReal&, Volumes&);
 
 void CalculaCoeficientes (vecReal&, vecReal&, vecReal&, vecReal&,
-                          vecReal&, vecReal&);
+                          vecReal&, vecReal&, Volumes&);
+
+void MultiplicadorPrincipal (vecReal&, vecReal&, vecReal&, vecReal&);
+
+void MultiplicadorVetSol (vecReal&, vecReal&, vecReal&, vecReal&, vecReal&);
+
+void Solver (vecReal&, vecReal&, vecReal&);
+
+
+
 
 
 
@@ -56,27 +65,62 @@ int main(int argc, char** argv)
     Volumes v1;    
     LeituraDadosProblema (v1);
     
-    std :: vector <Real>      xFronteiras (v1.NVOL()+1), //localizações fronteiras
-                              xCentro     (v1.NVOL()),   //localizações centros
-                              DistCentro  (v1.NVOL()+1), //distâncias entre centros
-                              DistFace    (v1.NVOL());   //distância entre fronteiras adjacentes
+    std :: vector <Real>             xFronteiras (v1.NVOL()+1), //localizações fronteiras
+                                     xCentro     (v1.NVOL()),   //localizações centros
+                                     DistCentro  (v1.NVOL()+1), //distâncias entre centros
+                                     DistFace    (v1.NVOL());   //distância entre fronteiras adjacentes
    
 
+//------------------------------------------------------------------------------
+//          Gerando malha a partir dos dados obtidos do arquivo externo
+//------------------------------------------------------------------------------
     GeracaoMalha (xFronteiras, xCentro, DistCentro, DistFace, v1);
     
-    std :: vector <Real>      Ap(v1.NVOL()),
-                              Ae(v1.NVOL()),
-                              Aw(v1.NVOL()),
-                              Sp(v1.NVOL());
     
-    CalculaCoeficientes (DistFace, DistCentro, Ap, Ae, Aw, Sp);
+//------------------------------------------------------------------------------
+//     Calculando coeficientes dos vetores para preparar a solução do sistema
+//------------------------------------------------------------------------------
     
+    std :: vector <Real>             Ap (v1.NVOL()),
+                                     Ae (v1.NVOL()),
+                                     Aw (v1.NVOL()),
+                                     Sp (v1.NVOL());
+    
+    CalculaCoeficientes (DistFace, DistCentro, Ap, Ae, Aw, Sp, v1);
+    
+//------------------------------------------------------------------------------
+//   Calculando multiplicadores após operações para resolução do sistema
+//------------------------------------------------------------------------------    
+       
+    std :: vector <Real>            MultP (v1.NVOL()), 
+                                    MultQ (v1.NVOL());
+    
+            
+    MultiplicadorPrincipal (MultP, Aw, Ae, Ap);
+    
+    MultiplicadorVetSol (MultQ, MultP, Sp, Ap, Aw);
+    
+//------------------------------------------------------------------------------
+//                    Calculando solução do sistema linear
+//------------------------------------------------------------------------------
+    
+    std :: vector <Real>            Solution (v1.NVOL());            
+    
+    Solver (MultQ, MultP, Solution);
+
+//------------------------------------------------------------------------------
+//                         Impressão de resultados
+//------------------------------------------------------------------------------
     for (int i=0; i<v1.NVOL(); i++)
     {
-    std :: cout << "Ae[" << i << "]=" << Ap[i] << std :: endl;
+        std :: cout << "Solution[" << i << "]=" << Solution[i] << std :: endl;
     } 
-    
-}    
+}   
+
+
+
+
+
 //==============================================================================
 //                           funções auxiliares
 //==============================================================================
@@ -155,7 +199,9 @@ int main(int argc, char** argv)
  }
  
  
- 
+//------------------------------------------------------------------------------
+//              2. Função para gerar malha envolvendo o intervalo dado
+//------------------------------------------------------------------------------ 
  
 void GeracaoMalha(vecReal& xfronteira, vecReal& xcentro, vecReal& distcentro,
                   vecReal& distface, Volumes& _vol) 
@@ -201,9 +247,11 @@ void GeracaoMalha(vecReal& xfronteira, vecReal& xcentro, vecReal& distcentro,
 }
 
  
-
+//------------------------------------------------------------------------------
+//          3. função para armazenar coeficientes dos vetores em questão
+//------------------------------------------------------------------------------
 void CalculaCoeficientes (vecReal& distf, vecReal& distc, vecReal& ap, 
-                          vecReal& ae, vecReal& aw, vecReal& sp)
+                          vecReal& ae, vecReal& aw, vecReal& sp, Volumes& _v)
 {
     
 //__________________1.Completando vetor diagonal oeste__________________________
@@ -220,10 +268,62 @@ void CalculaCoeficientes (vecReal& distf, vecReal& distc, vecReal& ap,
        else ae[i] = -1/distc[i+1];
    }
   
-//__________________1.Completando vetor diagonal principal______________________
+//__________________3.Completando vetor diagonal principal______________________
     for (int i=0; i<distf.size(); i++)
     {
         ap[i] = (1/distc[i+1]) + (1/distc[i]);
     }   
     
+//__________________4.Completando vetor solução_________________________________
+    for (int i=0; i<distf.size(); i++)
+    {
+        if (i==0) sp[i]=2*_v.IMGESQUERDA();
+        else if (i==distf.size()-1) sp[i]=2*_v.IMGDIREITA();
+        else sp[i] =0;
+    }   
+}
+
+//------------------------------------------------------------------------------
+//    4. Função para armazenar o vetor de multiplicadores da diagonal principal
+//------------------------------------------------------------------------------
+void MultiplicadorPrincipal (vecReal& vetP, vecReal& vetOeste, vecReal& vetLeste, 
+        vecReal& vetDiagPrinc)
+{    
+//__________________1.Completando vetor fator multiplicador P___________________
+    for (int i=0; i<vetDiagPrinc.size(); i++)
+    {
+        if (i==0) vetP[i]=vetLeste[i]/vetDiagPrinc[i];
+        
+        else vetP[i] = vetLeste [i]/(vetDiagPrinc[i] - vetP [i-1]*vetOeste[i]);
+    }
+    
+}
+
+//------------------------------------------------------------------------------
+//    5. Função para armazenar o vetor de multiplicadores do vetor solução
+//------------------------------------------------------------------------------
+void MultiplicadorVetSol (vecReal& vetQ, vecReal& vetP, vecReal& vetSol, 
+        vecReal& vetDiagPrinc,   vecReal& vetOeste)
+{
+     for (int i=0; i<vetDiagPrinc.size(); i++) 
+     {
+        if (i==0) vetQ[i]=vetSol[i]/vetDiagPrinc[i];
+
+        else vetQ[i] = (vetSol[i] - vetQ[i-1] * vetOeste[i])/
+                    (vetDiagPrinc[i] - vetP[i-1]*vetOeste[i]);
+     }
+}
+
+//------------------------------------------------------------------------------
+//           6. Função para calcular a solução do sistema linear
+//------------------------------------------------------------------------------
+void Solver (vecReal& vetQ, vecReal& vetP, vecReal& vetResultado)
+{
+    for (int i=vetQ.size()-1; i>=0; --i)
+    {
+        if(i==vetQ.size()-1) vetResultado[i]=vetQ[i];
+        
+        else vetResultado[i]= vetQ[i] - vetP[i]*vetResultado[i+1];
+        
+    }
 }
